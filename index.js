238137e111890e13dcbeb0ab02e2d7dca8e435b5
Ascol57 <ascol57@yahoo.com>
2023-12-05 20:45:11 +0100

    Adding a music exemple and starting parsing it

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const xml2js = require('xml2js');
+const fs = require('fs');
+const parser = new xml2js.Parser({ attrkey: "ATTR" });
+
+
 
 app.use('/', express.static('public'))
 
+app.get('/get', async (req, res) => {
+    file = fs.readFileSync('exemple.musicxml', 'utf8');
+
+    parser.parseString(file, function (error, result) {
+        if (error === null) {
+            console.log(result)
+
+            res.send(file)
+        }
+        else {
+            console.log(error);
+        }
+    });
+})
+
 app.listen(port, () => {
-  console.log(`Debussy static server listening on port ${port}`)
+    console.log(`Debussy static server listening on port ${port}`)
 })
